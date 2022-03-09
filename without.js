8f11f429d99b697d53e974fb5e135950629d7d94
Shraddha Naidu <naidu.shraddha@gmail.com>
2022-03-09 11:42:20 -0700

    Function that removes elements from a given array

---
+++ without.js
@@ -0,0 +1,36 @@
+//FUNCTION IMPLEMENTATION
+let eqArrays = function(arr1, arr2) {
+  for(let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+      }
+  }
+  return true;
+};
+
+let assertArraysEqual = function(arr1, arr2) {
+  if (!eqArrays(arr1, arr2)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+//given two arrays, remove second array from first
+//can use .includes method
+//push remaining elements into a new array
+
+let without = function(origArr, remArr) {
+  let newArray = [];
+
+  for (let element of origArr) {
+    if (!remArr.includes(element)) {
+      newArray.push(element)
+    }
+  }
+  return newArray;
+};
+
+//TEST CODE
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
