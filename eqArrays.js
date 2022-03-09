a28f8c6bddf9284b2d7492237ff2de379edf0b9a
Shraddha Naidu <naidu.shraddha@gmail.com>
2022-03-08 22:38:19 -0700

    Function that compares two arrays and returns a boolean

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Add a for loop if where each element is looped through and compared in each array
+
+let eqArrays = function(arr1, arr2) {
+  for(let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+      }
+  }
+  return true;
+
+};
+
+
+
+
+//TEST CODE
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
