a4e941c01a37e4bc79df8986c40a3dbfdeaac009
Shraddha Naidu <naidu.shraddha@gmail.com>
2022-03-14 12:24:35 -0600

    Function return key name for a given value

---
+++ findKey.js
@@ -0,0 +1,33 @@
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Function that return key name that matches value within an object
+//Check if Object.keys works w/out in loop
+
+const findKey = function(object, callback) {
+
+  for (let key in object) {
+    if (callback(object[key])) {
+      //console.log(key);
+      return key;
+    }
+  }
+
+};
+
+
+//TEST CODE
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
