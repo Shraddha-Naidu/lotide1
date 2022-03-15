d875f58a8fa9414f018d0f2413393f96ea10bee3
Shraddha Naidu <naidu.shraddha@gmail.com>
2022-03-14 22:27:57 -0600

    Removed test code and refactored using module.exports

--- middle.js
+++ middle.js
@@ -1,27 +1,6 @@
-let eqArrays = function(arr1, arr2) {
-  for(let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-      }
-  }
-  return true;
-};
+const assertArraysEqual = require('../assertArraysEqual')
 
-let assertArraysEqual = function(actual, expected) {
-  if (!eqArrays(actual, expected)) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-//Return new array of either a single element or two
-//if the array.length is even then return two middle values
-//if the array.length is odd return one middle value
-//if array.length is 2 or less return an []
-
-
-let middle = function(arr) {
+const middle = function(arr) {
   if (arr.length <= 2) {
     return [];
   } else if (arr.length % 2 === 0) {
@@ -29,10 +8,5 @@ let middle = function(arr) {
   }
 };
 
-//TEST CODE
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-console.log(middle([1, 2])); // => []
 
+module.exports = middle;
