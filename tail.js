8d89e710688e738fa9b16f46430c359142a6138f
Shraddha Naidu <naidu.shraddha@gmail.com>
2022-03-15 00:06:33 -0600

    Refactored for mocha/chai

--- tail.js
+++ tail.js
@@ -1,8 +1,3 @@
-//FUNCTION IMPLEMENTATION
-const assertEqual = require('../assertEqual')
-
-//function should return all ellements except for element[0] (.slice(1))
-
 const tail = function(array) {
   let newArray = array.slice(1);
   return newArray;
