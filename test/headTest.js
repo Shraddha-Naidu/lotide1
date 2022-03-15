2e290bf4d3f992f8e756900ec56be1d2b518bbb2
Shraddha Naidu <naidu.shraddha@gmail.com>
2022-03-14 22:23:48 -0600

    Test Functions

---
+++ test/headTest.js
@@ -0,0 +1,6 @@
+const head = require('../head');
+const assertEqual = require('./assertEqual');
+
+//TEST CODE
+assertEqual(head([00,18,7]), 5);
+assertEqual(head(["Parish", "Cowens", "Brown"]), "Hello");
